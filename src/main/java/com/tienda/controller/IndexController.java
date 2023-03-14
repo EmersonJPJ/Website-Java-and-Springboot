613e3069bd71b06186bc07ae810826c7db2901dc
emers <emers@DESKTOP-8RLOVHM>
2023-03-13 20:47:48 -0600

    Semana 8, Boostrap

---
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -0,0 +1,20 @@
+package com.tienda.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+/** 
+ * @author emers
+ */
+
+@Controller
+public class IndexController {
+    
+    @GetMapping("/")
+    public String inicio(Model model){      
+        return "index";
+    }
+            
+    
+   
+}
